9976885de1cbd0229d57f09459cfb4832d1605cd
James Cotton <peabody124@gmail.com>
2021-03-28 17:14:10 -0500

    Add MMPose TopDown detector
    Hardcoded right now to use HRnet + DarkPose
        hrnet_w48_coco_384x288_dark-e881a4b6_20210203.pth
    
    which should be placed in
        3rdparty/checkpoints/
    
    https://mmpose.readthedocs.io/en/latest/top_down_models.html#distribution-aware-coordinate-representation-for-human-pose-estimation
    https://download.openmmlab.com/mmpose/top_down/hrnet/hrnet_w48_coco_384x288_dark-e881a4b6_20210203.pth

--- pose_pipeline/pipeline.py
+++ pose_pipeline/pipeline.py
@@ -613,3 +613,74 @@ class ExposePersonVideo(dj.Computed):
 
         os.remove(out_file_name)
         os.remove(video)
+
+@schema
+class MMPoseTopDownPerson(dj.Computed):
+    definition = """
+    -> PersonBbox
+    ---
+    keypoints          : longblob
+    """
+
+    def make(self, key):
+        
+        from mmpose.apis import init_pose_model, inference_top_down_pose_model
+        from tqdm import tqdm
+
+        mmpose_files = os.path.join(os.path.split(__file__)[0], '../3rdparty/mmpose/')
+        pose_cfg = os.path.join(mmpose_files, 'config/top_down/darkpose/coco/hrnet_w48_coco_384x288_dark.py')
+        pose_ckpt = os.path.join(mmpose_files, 'checkpoints/hrnet_w48_coco_384x288_dark-e881a4b6_20210203.pth')
+
+        video, tracks, keep_tracks = (Video * TrackingBbox * PersonBboxValid & key).fetch1('video', 'tracks', 'keep_tracks')
+
+        model = init_pose_model(pose_cfg, pose_ckpt)
+
+        cap = cv2.VideoCapture(video)
+
+        results = []
+        for idx in tqdm(range(len(tracks))):
+            bbox = [t['tlwh'] for t in tracks[idx] if t['track_id'] in keep_tracks][0]
+            bbox_wrap = {'bbox': bbox}
+            
+            ret, frame = cap.read()
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            
+            if bbox[2] == 0 or bbox[3] == 0 or not ret or frame is None:
+                results.append(np.zeros(17, 3))
+                
+            res = inference_top_down_pose_model(model, frame, [bbox_wrap])[0]
+            results.append(res[0]['keypoints'])
+
+        key['keypoints'] = np.asarray(results)
+
+        os.remove(video)
+        self.insert1(key)
+
+@schema
+class MMPoseTopDownPersonVideo(dj.Computed):
+    definition = """
+    -> MMPoseTopDownPerson
+    ---
+    output_video      : attach@localattach    # datajoint managed video file
+    """
+
+    def make(self, key):
+        
+        from pose_pipeline.utils.visualization import video_overlay, draw_keypoints
+
+        video = (BlurredVideo & key).fetch1('output_video')
+        keypoints = (MMPoseTopDownPerson & key).fetch1('keypoints')
+        
+        def overlay_fn(image, idx):
+            image = draw_keypoints(image, keypoints[idx])
+            return image
+
+        _, out_file_name = tempfile.mkstemp(suffix='.mp4')
+        video_overlay(video, out_file_name, overlay_fn, downsample=4)
+
+        key['output_video'] = out_file_name
+
+        self.insert1(key)
+
+        os.remove(out_file_name)
+        os.remove(video)        
