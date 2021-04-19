81eed94e1412cd0df136e317245073b7e2e15b9a
James Cotton <peabody124@gmail.com>
2021-04-19 16:49:17 +0000

    Export objects

--- pose_pipeline/__init__.py
+++ pose_pipeline/__init__.py
@@ -8,5 +8,6 @@ from .pipeline import MMPoseTopDownPerson, MMPoseTopDownPersonVideo
 from .pipeline import GastNetPerson, GastNetPersonVideo
 from .pipeline import PoseWarperPerson, PoseWarperPersonVideo
 from .pipeline import PoseFormerPerson
+from .pipeline import VIBEPerson, MEVAPerson
 
 from .env import add_path, set_environmental_variables
