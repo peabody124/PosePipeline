1da682ce21e0c2bf06cbd99ed973f586bc8aa994
James Cotton <peabody124@gmail.com>
2021-07-21 00:06:10 +0000

    Check in tool for compression

---
+++ pose_pipeline/utils/video_format.py
@@ -0,0 +1,24 @@
+from pose_pipeline.pipeline import Video
+import subprocess
+import tempfile
+import os
+
+
+def compress(fn):
+    import subprocess
+
+    _, temp = tempfile.mkstemp(suffix='.mp4')
+    subprocess.run(['ffmpeg', '-y', '-i', fn, '-c:v', 'libx264', '-b:v', '5M', temp])
+    return temp
+
+
+def insert_local_video(filename, video_start_time, local_path, video_project="TESTING", skip_duplicates=False):
+    """ Insert local video into the Pose Pipeline """
+
+    assert os.path.exists(local_path)
+
+    vid_struct = {'video_project': video_project, 'filename': filename,
+                    'start_time': video_start_time, 'video': local_path}
+
+    print(vid_struct)
+    Video().insert1(vid_struct, skip_duplicates=skip_duplicates)
