1751f403ee53e2a4767e9bdc0daacee9b21be0a6
James Cotton <peabody124@gmail.com>
2022-08-09 14:25:35 +0000

    Tracking: add autoannotate method

---
+++ pose_pipeline/utils/tracking.py
@@ -0,0 +1,21 @@
+
+from pose_pipeline import *
+
+def annotate_single_person(filt, subject_id=0, confirm=False):
+
+    keys = ((TrackingBbox & filt 'num_tracks=1') - pose_pipeline.PersonBboxValid).fetch('KEY')
+
+    if confirm:
+        print(f'Found {len(keys)} videos that can be auto-annotated with only one person present. Type Yes to confirm.')
+        response = input()
+        if response[0].upper() != 'Y':
+            print('Aborting')
+            break
+
+    for k in keys:
+        tracks = (TrackingBbox & k).fetch1('tracks')
+        track_id = np.unique([[t['track_id'] for t in t2] for t2 in tracks])
+        assert len(track_id) == 1, "Found two tracks, should not have"
+        k.update({'video_subject_id': subject_id, 'keep_tracks': track_id})
+        PersonBboxValid.insert1(k)
+        PersonBbox.populate(k')
