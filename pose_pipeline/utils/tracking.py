6f6b81e12b7ca7e656befbab23f6c5379feb26ac
James Cotton <peabody124@gmail.com>
2022-08-14 14:03:49 +0000

    Fix bug in automatic annotation

--- pose_pipeline/utils/tracking.py
+++ pose_pipeline/utils/tracking.py
@@ -15,8 +15,7 @@ def annotate_single_person(filt, subject_id=0, confirm=False):
 
     for k in keys:
         tracks = (TrackingBbox & k).fetch1("tracks")
-        track_id = np.unique([[t["track_id"] for t in t2] for t2 in tracks])
+        track_id = np.unique([[t["track_id"] for t in t2] for t2 in tracks if len(t2) > 0])
         assert len(track_id) == 1, "Found two tracks, should not have"
         k.update({"video_subject_id": subject_id, "keep_tracks": track_id})
         PersonBboxValid.insert1(k)
-        PersonBbox.populate(k)
